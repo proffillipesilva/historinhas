e7d7cf0aadf52b6dfac88b51e2a19e204e087d88
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2022-11-21 21:23:22 -0300

    Update ReviwerServiceImpl.java

--- src/main/java/com/fiec/lpiiiback/services/impl/ReviwerServiceImpl.java
+++ src/main/java/com/fiec/lpiiiback/services/impl/ReviwerServiceImpl.java
@@ -2,27 +2,48 @@ package com.fiec.lpiiiback.services.impl;
 
 import com.fiec.lpiiiback.models.dto.BookRequestDto;
 import com.fiec.lpiiiback.models.entities.Book;
+import com.fiec.lpiiiback.services.BookService;
 import com.fiec.lpiiiback.services.ReviwerService;
 import com.google.api.services.docs.v1.Docs;
+import com.google.api.services.drive.model.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class ReviwerServiceImpl implements ReviwerService {
     @Autowired
     GoogleDriveManager googleDriveManager;
+
+    @Autowired
+    BookService bookService;
     @Override
     public Book createDocument(BookRequestDto bookRequestDto) throws GeneralSecurityException, IOException {
 
         com.google.api.services.docs.v1.model.Document doc = new  com.google.api.services.docs.v1.model.Document();
-        doc.setTitle(bookRequestDto.getName());
-        try {
-            Docs.Documents.Create create = googleDriveManager.getInstanceDocs().documents().create(doc);
 
-            System.out.println(create);
+        doc.setTitle(bookRequestDto.getName());
+
+
+        try {
+            com.google.api.services.docs.v1.model.Document docResponse;
+            Docs.Documents.Create create = googleDriveManager.getInstanceDocs().documents().create(doc);
+            docResponse = create.execute();
+            Permission permission = new Permission()
+                    .setType("anyone")
+                    .setRole("writer");
+            googleDriveManager.getInstance().permissions().create(docResponse.getDocumentId(), permission).execute();
+
+            //String DOCUMENT_ID = "prd" + UUID.randomUUID() + "_" + Long.toHexString(new Date().getTime());
+            //com.google.api.services.docs.v1.model.Document response = googleDriveManager.getInstanceDocs().documents().get(DOCUMENT_ID).execute();
+            //String title = response.getTitle();
+
+
+            return bookService.insertNewBook(bookRequestDto, docResponse.getDocumentId(), "");
         } catch(Exception ex){
             System.out.println(ex.getMessage());
         }
